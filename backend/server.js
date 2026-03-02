00062859a52bcd7546d10bcb39e1a1f334063589
Datta Panchal <dattapanchal464@gmail.com>
2026-03-02 18:00:58 +0530

    Completed Login and Signup Functionallity

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,16 @@
 import express from 'express';
 import 'dotenv/config';
 import { connectDB } from './db/connectDB.js';
+import cors from 'cors';
 import userRoutes from './routes/user.routes.js';
 const app = express();
 const PORT = process.env.PORT;
 
 app.use(express.json());
+app.use(cors({
+    origin:"http://localhost:5173",
+    credentials:true
+}));
 
 app.use("/api/v1/user",userRoutes);
 
