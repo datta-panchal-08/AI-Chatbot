6cec976e8d15b437add4b3789b29511efb768210
Datta Panchal <dattapanchal464@gmail.com>
2026-03-03 15:09:57 +0530

    Started Fresh

--- frontend/src/Api/endpoint.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const axiosInstance = axios.create({
-    baseURL:"http://localhost:3400/api/v1",
-    withCredentials:true
-});
-
-export const post = (url,data)=>axiosInstance.post(url,data);
